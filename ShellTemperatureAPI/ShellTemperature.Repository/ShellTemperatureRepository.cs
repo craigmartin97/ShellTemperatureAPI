a9c8303deb631c60ec59588fcde3453344f60df2
Craig Martin <c.martin0297@gmail.com>
2020-03-25 19:33:13 +0000

    Edited create shell temp method to check if device exists against device address field

--- ShellTemperatureAPI/ShellTemperature.Repository/ShellTemperatureRepository.cs
+++ ShellTemperatureAPI/ShellTemperature.Repository/ShellTemperatureRepository.cs
@@ -1,4 +1,5 @@
-﻿using ShellTemperature.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ShellTemperature.Data;
 using ShellTemperature.Repository.Interfaces;
 using System;
 using System.Threading.Tasks;
@@ -12,13 +13,22 @@ namespace ShellTemperature.Repository
         #endregion
 
         #region Create
+        /// <summary>
+        /// Create a new shell temperature in the database
+        /// </summary>
+        /// <param name="model">Model item to create</param>
+        /// <returns>Returns true if the model was successfully created</returns>
         public async Task<bool> Create(ShellTemp model)
         {
             if (model?.Device == null)
                 throw new ArgumentNullException(nameof(model), "The model supplied was invalid");
 
-            DeviceInfo dbDevice = await Context.DevicesInfo.FindAsync(model.Device.Id);
-            DeviceInfo device = dbDevice ?? model.Device;
+            // Try and find the device in the database
+            DeviceInfo dbDevice = await Context.DevicesInfo.FindAsync(model.Device.Id) ??
+                                  await Context.DevicesInfo.FirstOrDefaultAsync(dev =>
+                                                dev.DeviceAddress.Equals(model.Device.DeviceAddress));
+
+            DeviceInfo device = dbDevice ?? model.Device; // Use the database device or add models device
             model.Device = device;
 
             await Context.AddAsync(model);
